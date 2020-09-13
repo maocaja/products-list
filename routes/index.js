675543640ce21836a9a81e5943d2fad5a3aa811a
Mauricio Cajamarca <lmauriciocajamarca@gmail.com>
2020-09-13 17:47:31 -0500

    List of products

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,6 @@ var express = require('express');
 var router = express.Router();
 var indexController = require('../controllers/index');
 
-var items =[
-    {id: 1, name: 'product1'},
-    {id: 2, name: 'product2'},
-    {id: 3, name: 'product3'}
-];
-
-
 router.get('/', indexController.index);
 
 router.get('/products', indexController.listOfProducts);
